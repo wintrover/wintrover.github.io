058e5c2faaf9c58e012e247c27b1f4763c09b70c
wintrover <wintrover@gmail.com>
2025-03-04 22:56:58 +0900

    highlight.js로 변경

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('pre').forEach(pre => {
-        const button = document.createElement('button');
-        button.classList.add('copy-btn');
-        button.textContent = 'Copy';
+    // Highlight.js 초기화
+    document.querySelectorAll('pre code').forEach(block => {
+        hljs.highlightElement(block);
+    });
 
-        pre.style.position = 'relative';
-        pre.appendChild(button);
+    // Copy 버튼 로직
+    document.querySelectorAll('.code-container').forEach(container => {
+        const pre = container.querySelector('pre');
+        const code = pre.querySelector('code');
+        const button = container.querySelector('.copy-btn');
 
         button.addEventListener('click', () => {
-            const code = pre.querySelector('code').innerText;
-
-            navigator.clipboard.writeText(code).then(() => {
+            navigator.clipboard.writeText(code.innerText).then(() => {
                 button.textContent = 'Copied!';
                 setTimeout(() => button.textContent = 'Copy', 2000);
             }).catch(err => console.error('Failed to copy:', err));
